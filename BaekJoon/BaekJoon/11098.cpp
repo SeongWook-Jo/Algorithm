2536ed82103fc70f5ec166e34f89eab6f38b2e18
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2022-10-10 21:46:31 +0900

    11098
    string사용

--- BaekJoon/BaekJoon/11098.cpp
+++ BaekJoon/BaekJoon/11098.cpp
@@ -1,8 +1,39 @@
 #include<iostream>
-
+#include<cstring>
+#include<string>
 using namespace std;
 
 int main(void)
 {
+	int testCount;
+	int cCount,exMoney;
+	int cMoney[100];
+	string cName;
+	string exName[100];
+		
+	cin >> testCount;
+
+
+	for (int j = 0; j < testCount;j++)
+	{
+		cin >> cCount;
+		exMoney = 0;
+		for (int i = 0; i < cCount; i++)
+		{
+			cin >> cMoney[i] >> cName;
+
+			if (exMoney < cMoney[i])
+			{
+				exMoney = cMoney[i];
+				exName[j] = cName;
+			}
+		}
+	}
+	for (int i = 0; i < testCount; i++)
+	{
+		cout << exName[i] << endl;
+	}
+
+
 	return 0;
 }
