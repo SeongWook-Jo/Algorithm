189b18f33d124ca3350b3656b122a648483e6bb0
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2022-12-21 15:35:34 +0900

    2741
    cout 보다 printf가 훨씬 빠름.

---
+++ BaekJoon/BaekJoon/2741.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+
+int main(void)
+{
+	int num;
+	cin >> num;
+
+	for (int i = 1; i <= num; ++i)
+	{
+		printf("%d\n", i);
+	}
+
+
+	return 0;
+}
