f66ddcddc23fbe9c9d6422233e43b36e0602df30
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-03-26 23:05:34 +0900

    9012 괄호 문자열 체크 + 특수문자

---
+++ Algorithm/Algorithm/9012.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<string.h>
+#include<vector>
+using namespace std;
+
+
+
+int main()
+{
+	int tc;
+	string str;
+	cin >> tc;
+	cin.ignore();
+	for (int i = 0; i < tc; i++)
+	{
+		cin >> str;
+		int vps = 0;
+		string answer = "YES";
+		for (int j = 0; j < str.size(); j++)
+		{
+			if (str[j] == '\(')
+				vps++;
+			else if (vps == 0 && str[j] == '\)')
+			{
+				answer = "NO";
+				break;
+			}
+			else if (str[j] == '\)')
+				vps--;
+		}
+		if (vps != 0)
+			cout << "NO" << endl;
+		else
+			cout << answer << endl;
+	}
+
+	return 0;
+}
