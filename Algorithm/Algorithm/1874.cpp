347774b60c6c93f8869e2a0791584a37f8eb2013
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-03-27 19:14:35 +0900

    1874 스택 수열 endl 사용하지 않기

---
+++ Algorithm/Algorithm/1874.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<string.h>
+#include<vector>
+#include<stack>
+using namespace std;
+
+
+
+int main()
+{
+	int target;
+	int currNum = 1;
+	vector<char> stChar;
+	int tc;
+	stack<int> st;
+	cin >> tc;
+	for (int i = 0; i < tc; i++)
+	{
+		cin >> target;
+		while (currNum <= target)
+		{
+			st.push(currNum++);
+			stChar.push_back('+');
+		}
+		if (st.top() == target)
+		{
+			st.pop();
+			stChar.push_back('-');
+		}
+		else
+		{
+			cout << "NO" << endl;
+			return 0;
+		}
+	}
+	for (int i = 0; i < stChar.size(); i++) cout << stChar[i] << '\n';
+	return 0;
+}
