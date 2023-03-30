842d0ad5fcc2b8bcbdef80846821af887ab0d333
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-03-30 12:18:08 +0900

    10845 큐 구현

---
+++ Algorithm/Algorithm/10845.cpp
@@ -0,0 +1,99 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int tc;
+int input;
+int queue[10001];
+int idx = 0;
+int lowIdx = 0;
+string command;
+
+void Push(int x)
+{
+	queue[idx++] = x;
+}
+void Pop()
+{
+	if (idx == lowIdx)
+	{
+		cout << -1 << '\n';
+		return;
+	}
+	
+	cout << queue[lowIdx++]<<'\n';
+}
+
+void Size()
+{
+	cout << idx - lowIdx << '\n';
+}
+void Empty()
+{
+	if (idx == lowIdx)
+	{
+		cout << 1 << '\n';
+		return;
+	}
+	
+	cout << 0 << '\n';
+}
+void Front()
+{
+	if (idx == lowIdx) {
+		cout << -1 << '\n';
+		return;
+	}
+	cout << queue[lowIdx] << '\n';
+}
+void Back()
+{
+	if (idx == lowIdx) {
+		cout << -1 << '\n';
+		return;
+	}
+	cout << queue[idx-1] << '\n';
+}
+
+
+
+int main()
+{
+	cin >> tc;
+	cin.ignore();
+	while (tc--)
+	{
+		cin >> command;
+		if (command == "pop")
+		{
+			Pop();
+		}
+		else if (command == "size")
+		{
+			Size();
+		}
+		else if (command == "empty")
+		{
+			Empty();
+		}
+		else if (command == "front")
+		{
+			Front();
+		}
+		else if (command == "back")
+		{
+			Back();
+		}
+		else if (command == "push")
+		{
+			int x;
+			cin >> x;
+			Push(x);
+		}
+	}
+
+
+	return 0;
+}
+
