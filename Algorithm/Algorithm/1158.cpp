4cdf057ed3881470cbd7787bda498be11721a4f9
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-03-30 21:54:07 +0900

    1158 요세푸스 수열 queue

---
+++ Algorithm/Algorithm/1158.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+using namespace std;
+
+
+
+int main()
+{
+	queue<int> qu;
+	int tc,term,tmp;
+	
+	cin >> tc >> term;
+
+	for (int i = 1; i <= tc; i++)
+	{
+		qu.push(i);
+	}
+
+	cout << '<';
+
+	while (qu.size() > 1)
+	{
+		for (int i = 0; i < term-1; i++)
+		{
+			tmp = qu.front();
+			qu.pop();
+			qu.push(tmp);
+		}
+		tmp = qu.front();
+		qu.pop();
+		cout << tmp << ", ";
+	}
+	cout << qu.front() << ">\n";
+
+
+	return 0;
+}
+
