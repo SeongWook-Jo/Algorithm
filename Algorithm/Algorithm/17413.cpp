f7aa695853361f055e5d9cefdf2b7395da1d8fda
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-04-01 13:56:14 +0900

    17413 단어뒤집기2

---
+++ Algorithm/Algorithm/17413.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+using namespace std;
+
+
+
+int main()
+{
+	string input;
+	stack<char> st;
+	bool bo = false;
+
+	getline(cin, input);
+	input += ' ';
+	for (int i = 0; i < input.size(); i++)
+	{
+		if (input[i] == '<')
+		{
+			while (!st.empty())
+			{
+				char temp = st.top();
+				st.pop();
+				cout << temp;
+			}
+			bo = true;
+			cout << input[i];
+		}
+		else if (bo)
+		{
+			cout << input[i];
+			if (input[i] == '>')
+				bo = false;
+		}
+		else if (input[i] == ' ')
+		{
+			while(!st.empty())
+			{
+				char temp = st.top();
+				st.pop();
+				cout << temp;
+			}
+			cout << ' ';
+		}
+		else
+		{
+			st.push(input[i]);
+		}
+	}
+
+	return 0;
+}
+
