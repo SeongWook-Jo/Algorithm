2919c6ab25a4da873e39cc7df0bab930f730f2dd
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-04-02 22:31:00 +0900

    10799 쇠막대기

---
+++ Algorithm/Algorithm/10799.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+
+int main()
+{
+	string tc;
+	int blockCount = 0;
+	int currBlockCount = 0;
+	cin >> tc;
+	
+	for (int i = 0; i < tc.size(); i++)
+	{
+		if (tc[i] == '(')
+		{
+			//st.push(tc[i]);
+			currBlockCount++;
+			blockCount++;
+		}
+		else
+		{
+			currBlockCount--;
+			if (tc[i - 1] == '(')
+			{
+				//레이저
+				blockCount += currBlockCount;
+				blockCount--;
+				continue;
+			}
+		}
+	}
+	cout << blockCount;
+
+	return 0;
+}
+
