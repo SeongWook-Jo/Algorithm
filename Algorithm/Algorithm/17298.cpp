5c783d51dce3c5ed6bfde5e65381c0d637c22401
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-04-03 17:39:00 +0900

    17298 오큰수 stack

---
+++ Algorithm/Algorithm/17298.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int seq[1000001];
+int output[1000001];
+
+int main()
+{
+	int tc;
+
+	stack<int> st;
+
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++)
+		cin >> seq[i];
+
+	st.push(-1);
+	for (int j = tc - 2; j >= 0; j--)
+	{
+		st.push(-1);
+		for (int k = j + 1; k < tc; k++)
+		{
+			if (seq[j] < seq[k]) {
+				st.pop();
+				st.push(seq[k]);
+				break;
+			}
+		}
+	}
+	while (!st.empty())
+	{
+		int num = st.top();
+		st.pop();
+		cout << num << ' ';
+	}
+	return 0;
+}
+
