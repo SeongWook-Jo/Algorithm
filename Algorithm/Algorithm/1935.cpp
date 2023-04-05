110bed2097269bb037a7273e71833dd09cfca8b2
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-04-05 12:16:26 +0900

    1935 후위연산자2 double자료형 사용 int 와 char 연산 cout을 통한 소수점 자리수 표현

---
+++ Algorithm/Algorithm/1935.cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+
+
+int main()
+{
+	int tc;
+	int operand[26];
+	int input;
+	string str;
+	stack<double> st;
+
+
+	cin >> tc;
+	cin >> str;
+
+	for (int i = 0; i < tc; i++)
+	{
+		cin >> input;
+		operand[i] = input;
+	}
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (isalpha(str[i]))
+		{
+			st.push(operand[str[i] - 'A']);
+			continue;
+		}
+		double tempFlt = st.top();
+		st.pop();
+		switch (str[i])
+		{
+			case '*':
+				tempFlt = st.top() * tempFlt;
+				break;
+			case '/':
+				tempFlt = st.top() / tempFlt;
+				break;
+			case '+':
+				tempFlt = st.top() + tempFlt;
+				break;
+			case '-':
+				tempFlt = st.top() - tempFlt;
+				break;
+			default:
+				break;
+		}
+		st.pop();
+		st.push(tempFlt);
+	}
+
+	cout << fixed;
+	cout.precision(2);
+	cout << st.top() << '\n';
+
+	return 0;
+}
+
