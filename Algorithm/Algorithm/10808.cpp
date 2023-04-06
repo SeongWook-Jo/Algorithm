53856476519e75c9fe1612aa0c1dcd8d8dbddb0d
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-04-06 19:19:59 +0900

    10808 문자열

---
+++ Algorithm/Algorithm/10808.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<string>
+#include<stack>
+#include<queue>
+using namespace std;
+
+
+
+int main()
+{
+	string str;
+	int alpha[26] = { 0 };
+
+	cin >> str;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		alpha[str[i] - 'a']++;
+	}
+	
+	for (int i = 0; i < 26; i++)
+		cout << alpha[i] << ' ';
+
+
+	return 0;
+}
+
