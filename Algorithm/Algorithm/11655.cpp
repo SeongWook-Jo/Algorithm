481512f4d0dba471b688d02e08cf4cd677316b08
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-04-09 16:06:29 +0900

    2743 단어길이재기, 11655 ROT13

---
+++ Algorithm/Algorithm/11655.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+
+
+int main()
+{
+	string str;
+
+	getline(cin, str);
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (isalpha(str[i])) 
+		{
+			if (str[i] >= 'a' && str[i] < 'm' || str[i] >= 'A' && str[i] < 'M')
+				str[i] += 13;
+			else
+				str[i] -= 13;
+		}
+	}
+	cout << str<<'\n';
+
+
+	return 0;
+}
+
---
+++ Algorithm/Algorithm/2743.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+
+
+int main()
+{
+	string str;
+	cin >> str;
+	cout << str.size()<<'\n';
+	return 0;
+}
+
