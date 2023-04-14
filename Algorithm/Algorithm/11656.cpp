f56c016413ac1e4b4896e318473e73e24e0613f7
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-04-14 17:59:29 +0900

    11656 접미사 algorithm Library - sort

---
+++ Algorithm/Algorithm/11656.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+
+
+int main()
+{
+	string input;
+	string temp[1000];
+	int length;
+	cin >> input;
+	
+	length = input.length();
+
+	for (int i = 0; i < length; i++)
+	{
+		temp[i] = input.substr(i);
+	}
+	sort(temp, temp + length);
+
+	for (int i = 0; i < length; i++)
+		cout << temp[i] << '\n';
+
+	return 0;
+}
+
