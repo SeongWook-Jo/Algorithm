4dce7e5327f0e714ff333889134c1183f690d411
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2023-04-16 21:12:06 +0900

    10430 나머지

---
+++ Algorithm/Algorithm/10430.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	int input[3];
+
+	for (int i = 0; i < 3; i++)
+		cin >> input[i];
+	cout << (input[0] + input[1]) % input[2] << '\n';
+	cout << ((input[0] % input[2]) + (input[1] % input[2])) % input[2] << '\n';
+	cout << (input[0] * input[1]) % input[2] << '\n';
+	cout << ((input[0] % input[2]) * (input[1] % input[2])) % input[2] << '\n';
+
+	return 0;
+}
+
