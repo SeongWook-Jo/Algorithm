8ef454a097dac8dd4a5c074ac5e8d853ff7b40da
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2024-10-11 22:43:24 +0900

    1003 DP

---
+++ Algorithm/Algorithm/1003.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    vector<pair<int, int>> f(41);
+
+    f[0] = { 1, 0 };
+    f[1] = { 0,1 };
+
+    for (int i = 2; i < 41; i++)
+    {
+        f[i].first = f[i - 1].first + f[i - 2].first;
+        f[i].second = f[i - 1].second + f[i - 2].second;
+    }
+
+    while (N--)
+    {
+        int v;
+
+        cin >> v;
+
+        cout << f[v].first << " " << f[v].second << endl;
+    }
+}
+
