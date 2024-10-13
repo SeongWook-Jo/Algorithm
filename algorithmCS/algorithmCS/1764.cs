fd7b2e519b276cb6ef0a8094b919bb12e1353048
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2024-10-13 12:38:04 +0900

    1764 hash 사용 및 단순 구현

---
+++ algorithmCS/algorithmCS/1764.cs
@@ -0,0 +1,33 @@
+﻿namespace Program
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            
+            var nd = int.Parse(input[0]);
+            var nb = int.Parse(input[1]);
+
+            var nameHash = new HashSet<string>();
+            var nameList = new List<string>();
+
+            for (int i = 0; i < nd + nb; i++)
+            {
+                var name = Console.ReadLine();
+
+                if (nameHash.Contains(name))
+                    nameList.Add(name);
+                else
+                    nameHash.Add(name);
+            }
+
+            nameList.Sort();
+
+            Console.WriteLine(nameList.Count);
+
+            foreach (var name in nameList)
+                Console.WriteLine(name);
+        }
+    }
+}
