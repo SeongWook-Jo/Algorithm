11fb15c5a200823ad9babf252a85a6f54ca65dff
JoSeongWook <31335jsw@gmail.com>
2024-10-14 17:19:30 +0900

    1931 그리디 - 최적조건 Sort

---
+++ algorithmCS/algorithmCS/1931.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        (int s, int e)[] meetings = new (int s, int e)[n];
+
+        for (int i = 0; i < meetings.Length; i++)
+        {
+            var input = Console.ReadLine().Split();
+            meetings[i].s = int.Parse(input[0]);
+            meetings[i].e = int.Parse(input[1]);
+        }
+
+        meetings = meetings.OrderBy(m => m.e).ThenBy(m => m.s).ToArray();
+
+        var lastEndTime = 0;
+        var meetingCnt = 0;
+        foreach (var meet in meetings)
+        {
+            if (meet.s >= lastEndTime)
+            {
+                meetingCnt++;
+                lastEndTime = meet.e;
+            }
+        }
+
+        Console.WriteLine(meetingCnt);
+    }
+}
