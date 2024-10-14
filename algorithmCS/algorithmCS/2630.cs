62be6a91c09011c946b7b9aad776d159467b009e
SeongWook-Jo <72059459+SeongWook-Jo@users.noreply.github.com>
2024-10-14 23:56:33 +0900

    2630 - 분할정복

---
+++ algorithmCS/algorithmCS/2630.cs
@@ -0,0 +1,64 @@
+﻿namespace Program
+{
+    public static class Program
+    {
+        static int white, blue;
+
+        static int[,] arr;
+
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            arr = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+
+                for (int j = 0; j < n; j++)
+                    arr[i, j] = int.Parse(input[j]);
+            }
+
+            Func(0, n, 0, n);
+
+            Console.WriteLine(white);
+            Console.WriteLine(blue);
+        }
+
+        static void Func(int startX, int endX, int startY, int endY)
+        {
+            var whiteCnt = 0;
+            var blueCnt = 0;
+
+            for (int i = startY; i < endY; i++)
+            {
+                for (int j = startX; j < endX; j++)
+                {
+                    if (arr[i, j] == 0) whiteCnt++;
+                    if (arr[i, j] == 1) blueCnt++;
+                }
+            }
+
+            var needCnt = Math.Pow(endX - startX, 2);
+
+            if (whiteCnt == needCnt)
+            {
+                white++;
+                return;
+            }
+            else if (blueCnt == needCnt)
+            {
+                blue++;
+                return;
+            }
+
+            int half = (endX - startX) / 2;
+
+            Func(startX, startX + half, startY, startY + half);
+            Func(endX - half, endX, startY, startY + half);
+            Func(startX, startX + half, endY - half, endY);
+            Func(endX - half, endX, endY - half, endY);
+        }
+    }
+}
