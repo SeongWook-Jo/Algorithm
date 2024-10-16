3777d8d6a0e09d3b2d5990007e911519aeb6d1e6
JoSeongWook <31335jsw@gmail.com>
2024-10-16 15:00:10 +0900

    9095 dfs 재귀

---
+++ algorithmCS/algorithmCS/9095.cs
@@ -0,0 +1,39 @@
+﻿namespace Program
+{
+    public static class Program
+    {
+        static int cnt;
+        public static void Main(string[] args)
+        {
+            var tc = int.Parse(Console.ReadLine());
+            List<int> results = new List<int>();
+            for (int i = 0; i < tc; i++)
+            {
+                var value = int.Parse(Console.ReadLine());
+                cnt = 0;
+                DFS(value);
+                results.Add(cnt);
+            }
+
+            foreach (int result in results)
+                Console.WriteLine(result);
+        }
+
+        static void DFS(int value)
+        {
+            if (value == 0)
+            {
+                cnt++;
+                return;
+            }
+
+            if (value >= 3)
+                DFS(value - 3);
+
+            if (value >= 2)
+                DFS(value - 2);
+
+            DFS(value - 1);
+        }
+    }
+}
