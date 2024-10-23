91a92dbf79370bdb40ac7afd129c75962fb20472
JoSeongWook <31335jsw@gmail.com>
2024-10-23 14:01:51 +0900

    11399 그리디, 정렬

---
+++ algorithmCS/algorithmCS/11399.cs
@@ -0,0 +1,30 @@
+﻿namespace Program
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var values = new int[n];
+
+            var inputValues = Console.ReadLine().Split();
+
+            for (int i = 0; i < values.Length; i++)
+                values[i] = int.Parse(inputValues[i]);
+
+            values = values.OrderBy(v => v).ToArray();
+
+            var result = 0;
+            var sumValue = 0;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                result += sumValue + values[i];
+                sumValue += values[i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
