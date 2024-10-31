70f76567e05a0fa52b53f588dce3241f4b7834e5
JoSeongWook <31335jsw@gmail.com>
2024-10-31 10:58:47 +0900

    11659 - 구간합, sb

---
+++ algorithmCS/algorithmCS/11659.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Program
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+
+            var numCnt = int.Parse(input[0]);
+            var sumCnt = int.Parse(input[1]);
+
+            var nums = new int[numCnt + 1];
+
+            var numInput = Console.ReadLine().Split();
+
+            for (int i = 1; i < numCnt + 1; i++)
+                nums[i] = int.Parse(numInput[i - 1]);
+
+            var sums = new int[numCnt + 1];
+
+            for (int i = 1; i < numCnt + 1; i++)
+                sums[i] = nums[i] + sums[i - 1];
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < sumCnt; i++)
+            {
+                var numPos = Console.ReadLine().Split();
+
+                var start = int.Parse(numPos[0]);
+                var end = int.Parse(numPos[1]);
+
+                var result = sums[end] - sums[start-1];
+
+                sb.AppendLine(result.ToString());
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
